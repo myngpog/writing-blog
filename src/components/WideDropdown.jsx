fed4377c9b9116a1fd680786f31683fa6ef28190
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2025-07-30 04:00:34 -0700

    Fix hover on mobile

--- src/components/WideDropdown.jsx
+++ src/components/WideDropdown.jsx
@@ -2,13 +2,13 @@ import { wideButtonClassNames } from "./WideLink";
 
 export default function WideDropdown({ children, text }) {
   return (
-    <div class="relative group">
+    <div className="relative group">
       <button
-        class={`${wideButtonClassNames} text-dark-pink peer w-full group-hover:rounded-b-none`}
+        className={`${wideButtonClassNames} text-dark-pink peer w-full cursor-pointer group-hover:rounded-b-none`}
       >
         {text}
       </button>
-      <div class="flex flex-col absolute w-full z-1 group-hover:h-auto duration-300 -translate-y-2 group-hover:translate-y-0 transition-all dark-pink-button h-0 overflow-hidden group-hover:border-b rounded-b-md bg-background border-x border-dark-pink-button text-dark-pink [&>a]:w-full [&>a]:hover:underline [&>a]:text-center [&>a]:py-1.5">
+      <div className="flex flex-col absolute w-full z-1 group-hover:h-auto duration-300 -translate-y-2 group-hover:translate-y-0 transition-all dark-pink-button h-0 overflow-hidden group-hover:border-b rounded-b-md bg-background border-x border-dark-pink-button text-dark-pink [&>a]:w-full [&>a]:hover:underline [&>a]:text-center [&>a]:py-1.5">
         {children}
       </div>
     </div>
