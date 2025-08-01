13aa284816d3971649242806516b264b30c4fc36
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2025-08-01 00:45:52 -0700

    Lower WideDrodown max-h

--- src/components/WideDropdown.jsx
+++ src/components/WideDropdown.jsx
@@ -8,7 +8,7 @@ export default function WideDropdown({ children, text }) {
       >
         {text}
       </button>
-      <div className="flex flex-col absolute w-full z-1 group-hover/dropdown:max-h-55 duration-300 -translate-y-2 group-hover/dropdown:translate-y-0 transition-all dark-pink-button max-h-0 overflow-hidden group-hover/dropdown:border-b rounded-b-md bg-background border-x border-dark-pink-button text-dark-pink [&>a]:w-full [&>a]:hover:underline [&>a]:text-center [&>a]:py-1.5">
+      <div className="flex flex-col absolute w-full z-1 group-hover/dropdown:max-h-20 duration-300 -translate-y-2 group-hover/dropdown:translate-y-0 transition-all dark-pink-button max-h-0 overflow-hidden group-hover/dropdown:border-b rounded-b-md bg-background border-x border-dark-pink-button text-dark-pink [&>a]:w-full [&>a]:hover:underline [&>a]:text-center [&>a]:py-1.5">
         {children}
       </div>
     </div>
