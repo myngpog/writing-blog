2ce7b2ea2a508c180323653429cc95d919b7aa41
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2025-08-01 00:28:56 -0700

    Remove hamburger menu navbar. Add collapsible navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./components/SideBar";
 import Banner from "./components/Banner";
 import quotes from "../public/assets/quotes";
-import MobileSidebar from "./components/MobileSideBar";
 
 export default function App() {
   const [quote] = useState(() => quotes[Math.floor(Math.random() * quotes.length)]);
@@ -12,10 +11,9 @@ export default function App() {
   return (
     <>
       <Banner quote={quote} />
-      <div className="flex flex-row gap-10 xl:gap-22 ml-6 mr-6 xl:mr-15">
-        <Sidebar className="hidden lg:flex"/>
-        <MobileSidebar />
-        <div className="flex-1 mt-14">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-10 xl:gap-22 ml-6 mr-6 xl:mr-15">
+        <Sidebar />
+        <div className="flex-1 md:mt-14">
           <Outlet />
         </div>
       </div>
--- src/components/WideDropdown.jsx
+++ src/components/WideDropdown.jsx
@@ -2,13 +2,13 @@ import { wideButtonClassNames } from "./WideLink";
 
 export default function WideDropdown({ children, text }) {
   return (
-    <div className="relative group">
+    <div className="relative group/dropdown">
       <button
-        className={`${wideButtonClassNames} text-dark-pink peer w-full cursor-pointer group-hover:rounded-b-none`}
+        className={`${wideButtonClassNames} text-dark-pink peer w-full cursor-pointer group-hover/dropdown:rounded-b-none`}
       >
         {text}
       </button>
-      <div className="flex flex-col absolute w-full z-1 group-hover:max-h-96 duration-300 -translate-y-2 group-hover:translate-y-0 transition-all dark-pink-button max-h-0 overflow-hidden group-hover:border-b rounded-b-md bg-background border-x border-dark-pink-button text-dark-pink [&>a]:w-full [&>a]:hover:underline [&>a]:text-center [&>a]:py-1.5">
+      <div className="flex flex-col absolute w-full z-1 group-hover/dropdown:max-h-55 duration-300 -translate-y-2 group-hover/dropdown:translate-y-0 transition-all dark-pink-button max-h-0 overflow-hidden group-hover/dropdown:border-b rounded-b-md bg-background border-x border-dark-pink-button text-dark-pink [&>a]:w-full [&>a]:hover:underline [&>a]:text-center [&>a]:py-1.5">
         {children}
       </div>
     </div>
