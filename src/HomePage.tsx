ee6d563141210c685c54553b136c26790d6eddaf
myngpog <pingpengpongpog@gmail.com>
2026-04-21 15:24:21 -0700

    penguin gifP

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -45,7 +45,11 @@ export default function HomePage() {
       </section>
       <section className="w-full">
         <Heading className="pb-5 tracking-[0.2rem] lg:tracking-[0.42rem]">
-          currently writing
+          currently writing  <img
+            src="/assets/penguin-write.gif"
+            alt="penguin writing"
+            className="h-8 w-8"
+          />
         </Heading>
         <ul className="list-disc pl-10 text-text-light">
           <li>
