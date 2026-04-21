ac54282a9b6bcb3be947fea5387a8b598a4a8fad
myngpog <pingpengpongpog@gmail.com>
2026-04-21 15:27:35 -0700

    fixed the penguin gif

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -44,9 +44,9 @@ export default function HomePage() {
         </p>
       </section>
       <section className="w-full">
-        <Heading className="pb-5 tracking-[0.2rem] lg:tracking-[0.42rem]">
+        <Heading className="flex items-center pb-5 tracking-[0.2rem] lg:tracking-[0.42rem]">
           currently writing  <img
-            src="/assets/penguin-write.gif"
+            src="../penguin-write.gif"
             alt="penguin writing"
             className="h-8 w-8"
           />
